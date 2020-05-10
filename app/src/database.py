a076c282759f33c412a095538cca07f26d2aed2e
Carlos Silva <carloshenriquetorress@gmail.com>
2020-05-10 10:52:04 -0300

    Refactor database.py

--- app/src/database.py
+++ app/src/database.py
@@ -2,81 +2,83 @@ import sqlite3
 
 
 class Database:
-    __main = sqlite3.connect('db/main.db', check_same_thread=False)
-    __aux = sqlite3.connect('db/aux.db', check_same_thread=False)
+    __database = sqlite3.connect('db/twitter.db', check_same_thread=False)
     # TODO be careful, allowing multiple threads may run in concurrency issues
 
-    @classmethod
-    def close_database(cls):
-        Database.__aux.close()
-        Database.__main.close()
-
     @staticmethod
-    def setup():
-        try:
-            Database.__create_aux_tables()
-            Database.__create_table_tweet()
-        except sqlite3.OperationalError:
-            print('Fucked up the db')
-
-    @staticmethod
-    def __create_aux_tables():
-        try:
-            Database.__aux.execute('CREATE TABLE hashtags (hashtag text primary key)')
-        except sqlite3.OperationalError:
-            # TODO improve and add log
-            print("DATABASE ALREADY EXISTS!")
+    def database_setup():
+        Database.__table_tweet_create()
+        Database.__table_hashtags_create()
+        Database.__table_hashtags_fill()
+        print("All set and ready to go!")
 
     @classmethod
-    def __create_table_tweet(cls):
+    def database_close(cls):
+        Database.__database.commit()
+        print("Committing last changes")
+        Database.__database.close()
+        print("Bye!")
+        # TODO check if this may throws errors
+        #  I should apply TDD if have time
+        #  and convert to creator/destructor
+
+    @staticmethod
+    def __table_hashtags_create():
+        try:
+            Database.__database.execute('CREATE TABLE hashtags (hashtag text primary key)')
+            Database.__database.commit()
+        except sqlite3.OperationalError:
+            print("Could not create table")
+
+    @classmethod
+    def __table_tweet_create(cls):
         create = '''CREATE TABLE tweets (
-                    id text primary key,
-                    created_at date,
-                    text text,
-                    hashtag text,
-                    user text);'''
+                        id text primary key,
+                        created_at date,
+                        text text,
+                        hashtag text,
+                        user text
+                    )'''
         try:
-            Database.__main.execute(create)
+            Database.__database.execute(create)
+            Database.__database.commit()
         except sqlite3.OperationalError:
-            raise sqlite3.OperationalError('Could not create database!')
+            print("Could not create table")
 
     @staticmethod
-    def __fill_aux_table():
+    def __table_hashtags_fill():
         try:
             # TODO improve inserting/importing this data
-            Database.__aux.execute('''INSERT INTO hashtags
-            VALUES
-            ('openbanking'),
-            ('remediation'),
-            ('devops'),
-            ('sre'),
-            ('microservices'),
-            ('observability'),
-            ('oauth'),
-            ('metrics'),
-            ('logmonitoring'),
-            ('opentracing');
+            Database.__database.execute('''
+                INSERT INTO hashtags
+                VALUES
+                    ('openbanking'),
+                    ('remediation'),
+                    ('devops'),
+                    ('sre'),
+                    ('microservices'),
+                    ('observability'),
+                    ('oauth'),
+                    ('metrics'),
+                    ('logmonitoring'),
+                    ('opentracing')
             ''')
-            Database.__aux.commit()
+            Database.__database.commit()
         except sqlite3.IntegrityError:
-            # TODO ...
             print("HASHTAGS ALREADY CREATED!")
 
     @classmethod
     def hashtags(cls):
         try:
-            Database.__fill_aux_table()
+            return Database.__database.execute('SELECT hashtag FROM hashtags;').fetchall()
         except sqlite3.OperationalError:
-            # TODO treat this error
-            print("FIX ME! Database.hashtags")
-
-        return Database.__aux.execute('''SELECT hashtag FROM hashtags;''').fetchall()
-
+            print("Shit went bad")
 
     @classmethod
     def write_tweet(cls, tweet):
         try:
-            Database.__main.execute('INSERT INTO tweets VALUES (?, ?, ?, ?, ?)', tweet)
+            # TODO sanitize input
+            Database.__database.execute('INSERT INTO tweets VALUES (?, ?, ?, ?, ?)', tweet)
         except sqlite3.OperationalError:
             # TODO yeah
             print("Can't write tweet")
@@ -84,28 +86,27 @@ class Database:
     @classmethod
     def write_tweets(cls, tweets):
         for tweet in tweets:
-            # TODO sanitize data
             Database.write_tweet(tweet)
             # TODO tryout sqlite3.executemany()
-            # if it is more performatic or if it can tolerate failures
-            # in the data passed
+            #  if it is more performatic or if it can tolerate failures
+            #  in the data passed
 
-    @classmethod
-    def tweet(cls, id):
+    @staticmethod
+    def tweet(id):
         try:
-            Database.__main.execute('SELECT * FROM tweets WHERE id=?', id)
+            Database.__database.execute('SELECT * FROM tweets WHERE id=?', id)
         except sqlite3.OperationalError:
             print('Invalid ID')
-            return None
 
-    @classmethod
-    def tweets(cls, ids):
+    @staticmethod
+    def tweets(ids):
         tweets = []
         for id in ids:
             tweets.append(Database.tweet(id))
         # TODO check if execute many works here too
         return tweets
 
-    @classmethod
-    def all_tweets(cls):
-        return Database.__main.execute('SELECT * FROM tweets')
+    @staticmethod
+    def all_tweets():
+        # TODO try catch...
+        return Database.__database.execute('SELECT * FROM tweets').fetchall()
