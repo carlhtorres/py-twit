1068d360e33299b6eca478ca2ba8a78a018d62cd
Carlos Silva <carloshenriquetorress@gmail.com>
2020-05-10 11:02:00 -0300

    Refactor client.py

--- app/src/client.py
+++ app/src/client.py
@@ -2,20 +2,11 @@ import twitter
 
 
 class Twitter:
-    __api_key = "api key"
-    __api_key_secret = "api key secret"
+    __api_key = "__api key"
+    __api_key_secret = "__api key secret"
     __access_token_key = "access token key"
     __access_token_secret = "access token secret"
-    # TODO make this private WITHOUT breaking the rest
-    api = twitter.Api
-
-    @staticmethod
-    def search(query):
-        try:
-            return Twitter.api.GetSearch(raw_query=query, return_json=True)
-        except twitter.TwitterError:
-            # TODO log errors
-            print("FIX ME! Twitter.search")
+    __api = twitter.Api
 
     @staticmethod
     def __credentials(api_key, api_key_secret, access_token_key, access_token_secret):
@@ -30,12 +21,19 @@ class Twitter:
     def connect(api_key, api_key_secret, access_token_key, access_token_secret):
         Twitter.__credentials(api_key, api_key_secret, access_token_key, access_token_secret)
         try:
-            Twitter.api = twitter.Api(
+            Twitter.__api = twitter.Api(
                 consumer_key=api_key,
                 consumer_secret=api_key_secret,
                 access_token_key=access_token_key,
                 access_token_secret=access_token_secret
             )
         except twitter.TwitterError:
-            # TODO log everytime this is called
             print("FIX ME! Twitter.connect")
+
+    @staticmethod
+    def search(query):
+        try:
+            return Twitter.__api.GetSearch(raw_query=query, return_json=True)
+            # TODO handle timeouts and limits
+        except twitter.TwitterError:
+            print("FIX ME! Twitter.search")
